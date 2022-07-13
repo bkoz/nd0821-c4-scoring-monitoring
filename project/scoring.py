e99ad5aa56071f33456bb0d4514f3b23b51a7092
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-13 16:31:51 -0500

    training code

--- project/scoring.py
+++ project/scoring.py
@@ -9,17 +9,18 @@ from sklearn.linear_model import LogisticRegression
 import json
 
 
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-#################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
 
 
-#################Function for model scoring
+# Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
-
+    # this function should take a trained model, load test data
+    # and calculate an F1 score for the model relative to
+    # the test data it should write the result to the
+    # latestscore.txt file
+    pass
--- project/training.py
+++ project/training.py
@@ -1,32 +1,73 @@
-from flask import Flask, session, jsonify, request
+# from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
 import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
+import logging
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+logging.basicConfig(level=logging.DEBUG)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'])
+model_path = os.path.join(config['output_model_path'])
+logging.debug(f"dataset_csv_path: {dataset_csv_path}")
+logging.debug(f"model_path: {model_path}")
 
 
-#################Function for training the model
+# Function for training the model
 def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    # Read data and create a data frame
+    csv_file_path = os.getcwd() + "/" + dataset_csv_path + "/finaldata.csv"
+    dataframe = pd.read_csv(csv_file_path)
+    logging.debug(f"dataframe:\n {dataframe}")
 
+    # Create the training sets.
+    X = dataframe.loc[:, ['lastmonth_activity',
+                          'lastyear_activity',
+                          'number_of_employees']].values.reshape(-1, 3)
+    y = dataframe['exited'].values
+    logging.info(f"X = {X}")
+    logging.info(f"y = {y}")
+
+    # use this logistic regression for training
+    # BK - Changed multi_class from 'warn' to 'auto'.
+    logits = LogisticRegression(C=1.0, class_weight=None, dual=False,
+                                fit_intercept=True,
+                                intercept_scaling=1, l1_ratio=None,
+                                max_iter=100,
+                                multi_class='auto', n_jobs=None,
+                                penalty='l2',
+                                random_state=0, solver='liblinear',
+                                tol=0.0001, verbose=0,
+                                warm_start=False)
+
+    # fit the logistic regression to your data
+    model = logits.fit(X, y)
+
+    logging.info(f"predict = {model.predict(X)}")
+    logging.info(f"score = {model.score(X, y)}")
+
+    # write the trained model to your workspace in a
+    # file called trainedmodel.pkl
+    output_model_dir = os.getcwd() + "/" + model_path
+    output_model_path = output_model_dir + "/trainedmodel.pkl"
+    #
+    # Create the output_model_dir if it does not exist.
+    #
+    try:
+        (os.mkdir(output_model_dir))
+    except FileExistsError:
+        logging.info(f"{output_model_dir}\
+                     directory exists, skipping mkdir()")
+    file_handle = open(output_model_path, 'wb')
+    pickle.dump(model, file_handle)
+    file_handle.close()
+    logging.info(f"Saved model as {output_model_path}")
+
+
+if __name__ == "__main__":
+    train_model()
