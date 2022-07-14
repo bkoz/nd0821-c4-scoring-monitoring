576ea4fd27463ee46c649323371fae6927656802
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-14 16:20:25 -0500

    started diagnostics

--- project/diagnostics.py
+++ project/diagnostics.py
@@ -12,24 +12,38 @@ with open('config.json','r') as f:
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
 
-##################Function to get model predictions
-def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+# Function to get model predictions
+def model_predictions(dataset: pd.DataFrame) -> list:
+    """
+    This function should take an argument that consists of a dataset, 
+    in a pandas DataFrame format. It should read the deployed model from 
+    the directory specified in the prod_deployment_path entry of your 
+    config.json file.
 
-##################Function to get summary statistics
+    The function uses the deployed model to make predictions for each row 
+    of the input dataset. 
+    
+    The output should be a list of predictions. 
+    This list should have the same length as the number of rows in the 
+    input dataset.
+    """
+    # read the deployed model and a test dataset, calculate predictions
+    return # return value should be a list containing all predictions
+
+# Function to get summary statistics
 def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    # calculate summary statistics here
+    return # return value should be a list containing all summary statistics
 
-##################Function to get timings
+# Function to get timings
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    # calculate timing of training.py and ingestion.py
+    return # return a list of 2 timing values in seconds
 
-##################Function to check dependencies
+# Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    # get a list of
+    pass
 
 
 if __name__ == '__main__':
