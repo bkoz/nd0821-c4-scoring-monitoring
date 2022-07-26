15ad2a821922f9a19e01f2c158403a7f29c97239
Bob Kozdemba <bkozdemba@gmail.com>
2022-07-26 08:23:54 -0500

    working on api request code

--- project/apicalls.py
+++ project/apicalls.py
@@ -1,44 +1,46 @@
 import requests
 import logging
-import json
-import subprocess
-import os
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # Specify a URL that resolves to your workspace
 URL = "http://127.0.0.1:8000/"
 
 # Call each API endpoint and store the responses
 # status_code = requests.get(URL).status_code
-# response1 = requests.get(URL).content.decode('utf8').replace("'", '"').strip('/n')
-# logging.info(f"apicalls: GET / {response1}")
-# response2 = requests.get(URL + "scoring").content.decode('utf8').replace("'", '"').strip('/n')
-# response3 = requests.get(URL + "summarystats").content.decode('utf8').replace("'", '"').strip('/n')
-#response4 = requests.get(URL + "diagnostics").content.decode('utf8').replace("'", '"').strip('/n')
-data={"filename" : f'{os.getcwd()} + "/" + "/testdata/testdata.csv"'}
-data={'filename' : '/testdata/testdata.csv'}
-headers={'Content-Type': 'application/json'}
+response1 = requests.get(URL)\
+    .content.decode('utf8')\
+    .replace("'", '"').strip('/n')
+logging.info(f"apicalls: GET / {response1}")
+response2 = requests.get(URL + "scoring")\
+    .content.decode('utf8')\
+    .replace("'", '"').strip('/n')
+response3 = requests.get(URL + "summarystats")\
+    .content.decode('utf8')\
+    .replace("'", '"').strip('/n')
+response4 = requests.get(URL + "diagnostics")\
+                         .content.decode('utf8')\
+                         .replace("'", '"')\
+                         .strip('/n')
+data = {'filename': '/testdata/testdata.csv'}
+headers = {'Content-Type': 'application/json'}
 logging.debug(f"URL = {URL + 'prediction'}")
 logging.debug(f"data = {data}")
 logging.debug(f"headers = {headers}")
-response5 = requests.post(URL + "prediction",
+response5 = requests.post(
+                          URL + "prediction",
                           json=data,
-                          headers=headers).content.decode('utf8').replace("'", '"').strip('/n')
-
-# response5=subprocess.run(
-#     ['curl', '127.0.0.1:8000/prediction',
-#     '-d={"filename" : "/testdata/testdata.csv"}',
-#     '--header "Content-Type: application/json"'],
-#     capture_output=True).stdout
-print(f"response5 = {response5}")
+                          headers=headers
+                          )\
+                          .content.decode('utf8')\
+                          .replace("'", '"').strip('/n')
 
 # combine all API responses
 responses = []
-# responses.append(response1)
-# responses.append(response2)
-# responses.append(response3)
-#responses.append(response4)
+responses.append(response1)
+responses.append(response2)
+responses.append(response3)
+responses.append(response4)
 responses.append(response5)
 logging.info(f"apicalls: GET / {responses}")
 
@@ -46,5 +48,12 @@ logging.info(f"apicalls: GET / {responses}")
 for r in responses:
     print(r)
 
-
-
+# Save the responses to a file.
+response_file = "apireturns.txt"
+try:
+    file_handle = open(response_file, 'w')
+    for response in responses:
+        file_handle.write(response)
+    file_handle.close()
+except FileNotFoundError:
+    logging.error(f"Error creating score file: {response_file}")
--- project/app.py
+++ project/app.py
@@ -15,7 +15,7 @@ import json
 import os
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 ######################Set up variables for use in our script
 app = Flask(__name__)
